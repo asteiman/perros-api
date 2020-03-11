c5c5dda935e800a5346e1181418b53b589eb9968
Alan Steiman <asteiman@gmail.com>
2020-03-11 14:49:54 +0800

    Secure all routes

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -5,26 +5,21 @@ import Vapor
 public func routes(_ router: Router) throws {
     // public routes
     let userController = UserController()
-    router.post("users", use: userController.create)
-    
-    // Customers
-    router.get("customers", use: CustomerController().index)
-    router.get("customers", "mail", use: CustomerController().mail)
-    router.get("customers", Int.parameter, "billings", use: CustomerController().billing)
-    router.get("customers", Int.parameter, "invoices", use: CustomerController().invoice)
-    
-    // Invoices
-    router.get("invoices", use: InvoiceController().index)
-    router.get("invoices", Invoice.parameter, use: InvoiceController().single)
     
     // basic / password auth protected routes
     let basic = router.grouped(User.basicAuthMiddleware(using: BCryptDigest()))
     basic.post("login", use: userController.login)
     
     // bearer / token auth protected routes
-    let bearer = router.grouped(User.tokenAuthMiddleware())
-    let todoController = TodoController()
-    bearer.get("todos", use: todoController.index)
-    bearer.post("todos", use: todoController.create)
-    bearer.delete("todos", Todo.parameter, use: todoController.delete)
+    let bearer = router.grouped(User.tokenAuthMiddleware(), User.guardAuthMiddleware())
+    
+    // Customers
+    bearer.get("customers", use: CustomerController().index)
+    bearer.get("customers", "mail", use: CustomerController().mail)
+    bearer.get("customers", Int.parameter, "billings", use: CustomerController().billing)
+    bearer.get("customers", Int.parameter, "invoices", use: CustomerController().invoice)
+    
+    // Invoices
+    bearer.get("invoices", use: InvoiceController().index)
+    bearer.get("invoices", Invoice.parameter, use: InvoiceController().single)
 }
